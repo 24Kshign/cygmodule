ccd28bf14e607b98d084feac2063223b8233c9f6
jack <17764576259@163.com>
2017-06-20 15:06:52 +0800

    BaseObserver修改

--- modules/CygHttp/src/main/java/cn/share/jack/cyghttp/callback/CygBaseObserver.java
+++ modules/CygHttp/src/main/java/cn/share/jack/cyghttp/callback/CygBaseObserver.java
@@ -1,18 +1,23 @@
 package cn.share.jack.cyghttp.callback;
 
 import android.accounts.NetworkErrorException;
+import android.net.ParseException;
 import android.text.TextUtils;
-import android.widget.Toast;
+import android.util.Log;
 
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonSyntaxException;
 
-import java.io.IOException;
+import org.json.JSONException;
+
+import java.io.InterruptedIOException;
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.util.concurrent.TimeoutException;
 
 import cn.share.jack.cyghttp.ApiException;
-import cn.share.jack.cyghttp.app.CygApplication;
+import cn.share.jack.cyghttp.util.FRToast;
 
 /**
  * 对请求失败的分类操作
@@ -56,21 +61,25 @@ public abstract class CygBaseObserver<T> extends BaseObserver<T> {
     protected void onBaseError(Throwable t) {
         StringBuffer sb = new StringBuffer();
         sb.append("请求失败：");
-        if (t instanceof NetworkErrorException || t instanceof UnknownHostException) {
+        if (t instanceof NetworkErrorException || t instanceof UnknownHostException || t instanceof ConnectException) {
             sb.append("网络异常");
-        } else if (t instanceof SocketTimeoutException || t instanceof ConnectException
-                || t instanceof IOException) {
+        } else if (t instanceof SocketTimeoutException || t instanceof InterruptedIOException || t instanceof TimeoutException) {
             sb.append("请求超时");
         } else if (t instanceof JsonSyntaxException) {
-            sb.append("服务端数据解析失败");
+            sb.append("请求不合法");
+        } else if (t instanceof JsonParseException
+                || t instanceof JSONException
+                || t instanceof ParseException) {   //  解析错误
+            sb.append("解析错误");
         } else if (t instanceof ApiException) {
-            ApiException exception = (ApiException) t;
-            if (exception.isTokenExpried()) {
-                sb.append("token异常");
-            } else {
-                sb.append(String.valueOf(t.getMessage()));
+            if (((ApiException) t).isTokenExpried()) {
+                sb.append("Token出错");
             }
+        } else {
+            FRToast.showToastSafe(t.getMessage());
+            return;
         }
-        Toast.makeText(CygApplication.getInstance(), sb.toString(), Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "onBaseError: " + sb.toString());
+        FRToast.showToastSafe(sb.toString());
     }
 }
